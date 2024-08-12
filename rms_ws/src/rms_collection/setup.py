add8cfba2c94a7de3ee3ef4bc65a7f8d08eead4a
FirstNameLastName <FirstNameLastName@gmail.com>
2024-08-12 15:20:01 -0400

    add shell packages for collection and registration

---
+++ rms_ws/src/rms_collection/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'rms_collection'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='viperx250',
+    maintainer_email='FirstNameLastName@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
