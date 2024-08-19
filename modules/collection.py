3ac7163c2cf974a2319c6d22382a2c9a6d9d4ace
FirstNameLastName <FirstNameLastName@gmail.com>
2024-08-19 11:11:34 -0400

    manipulator collection service points towards object then goes home sleep

--- modules/collection.py
+++ modules/collection.py
@@ -10,6 +10,6 @@ def get_viewpoints(read_path):
         lines = file.readlines()
     data = []
     for line in lines:
-        values = [float(x) for x in line.strip.split(",")]
+        values = [float(x) for x in line.strip().split(",")]
         data.append(values)
     return np.asarray(data)
--- rms_ws/src/rms_nodes/rms_nodes/collect_at_viewpoints_server.py
+++ rms_ws/src/rms_nodes/rms_nodes/collect_at_viewpoints_server.py
@@ -7,10 +7,12 @@ from rms_msgs.srv import PCDCollection
 
 from os import path
 import sys
+import tf2_ros
 
 sys.path.append(path.abspath(path.join(path.dirname(__file__), "../../../../")))
 
 from modules.collection import *
+from modules.manipulators import *
 
 
 class ViewpointCollectionServer(Node):
@@ -27,6 +29,9 @@ class ViewpointCollectionServer(Node):
             "viewpoint_collection",
             self._execute_callback
         )
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
    
 
     def _execute_callback(self, request, response):
@@ -34,24 +39,25 @@ class ViewpointCollectionServer(Node):
         
         """
         manipulator = request.manipulator
+        read_path = request.read_path
 
 
-        # self._pcd_subscription = self.create_subscription(
-        #     PointCloud2,
-        #     f"/{manipulator}/camera/depth/color/points",
-        #     self._pcd_callback,
-        #     10
-        # )
+        self._pcd_subscription = self.create_subscription(
+            PointCloud2,
+            f"/{manipulator}/camera/depth/color/points",
+            self._pcd_callback,
+            10
+        )
 
-        # feedback_handle = PCDCollection.Feedback()
-        # feedback_handle.progress = 0.0
-        # manipulator = goal_handle.request.manipulator
-        # read_path = goal_handle.request.read_path
-        # write_directory = goal_handle.request.write_directory
+        viewpoints = get_viewpoints(read_path)
+        bot = get_vx_bot(manipulator)
 
-        # # get viewpoints
-        # viewpoints = get_viewpoints(read_path)
+        #
+        for view in viewpoints:
+            move_vx_bot_to_viewpoint(bot, x=view[1], y=view[2], z=view[3])
 
+        move_vx_bot_to_home(bot)
+        move_vx_bot_to_sleep(bot)
         #
         return response
     
